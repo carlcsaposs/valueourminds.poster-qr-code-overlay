4822e5721582c9f019a0c0c753d7f5c98b738c6d
ccsaposs <32380359+ccsaposs@users.noreply.github.com>
2020-07-26 21:13:44 -0700

    Change indent to 4 spaces

--- overlay_qr_code.py
+++ overlay_qr_code.py
@@ -20,17 +20,17 @@ BASE_POSTER = Image.open(INPUT_FILE_PATH).convert('RGB')
 
 POSTER_IMAGES = []
 for poster_number in range(LINK_ID_START, LINK_ID_END+1):
-  QR_DATA = qrcode.QRCode(
-    error_correction=qrcode.constants.ERROR_CORRECT_Q,
-    box_size=30,
-    border=0,
-  )
-  QR_DATA.add_data('https://link.valueourminds.com/poster/' + POSTER_TYPE + str(poster_number))
-  QR_DATA.make(fit=True)
-  QR_CODE_IMAGE = QR_DATA.make_image().resize((SIZE, SIZE))
-  POSTER_IMAGE = copy.deepcopy(BASE_POSTER)
-  POSTER_IMAGE.paste(QR_CODE_IMAGE, box=(X_COORDINATE, Y_COORDINATE))
-  POSTER_IMAGES.append(POSTER_IMAGE)
+    QR_DATA = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        box_size=30,
+        border=0,
+    )
+    QR_DATA.add_data('https://link.valueourminds.com/poster/' + POSTER_TYPE + str(poster_number))
+    QR_DATA.make(fit=True)
+    QR_CODE_IMAGE = QR_DATA.make_image().resize((SIZE, SIZE))
+    POSTER_IMAGE = copy.deepcopy(BASE_POSTER)
+    POSTER_IMAGE.paste(QR_CODE_IMAGE, box=(X_COORDINATE, Y_COORDINATE))
+    POSTER_IMAGES.append(POSTER_IMAGE)
 
 POSTER_IMAGES[0].save(OUTPUT_FILE_PATH, 'PDF', resolution=100.0, save_all=True, append_images=POSTER_IMAGES[1:])
 print('PDF exported to ' + OUTPUT_FILE_PATH)
